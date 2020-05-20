f92a72b1b5ace9bdb3dc984e6f28d5a3e3435926
aha <223311795@qq.com>
2020-05-20 18:15:10 +0800

    分类单元测试

--- test/type/api.test.js
+++ test/type/api.test.js
@@ -3,27 +3,50 @@
  */
 
 const server = require('../server')
-const { token } = require('../testInfo')
+const { token, typeData } = require('../testInfo')
 const { DEFAULT_LOGO } = require('../../src/config/constant')
 
-const typePostData = {
-  title: '测试标题',
-  logo: DEFAULT_LOGO,
-  index: 0
-}
+let typeId
 
 test('创建分类，应该成功', async () => {
   const res = await server
-    .post('/api/types/create')
-    .send(typePostData)
+    .post('/api/types')
+    .send(typeData)
     .set('Authorization', 'Bearer ' + token)
   expect(res.body.errno).toBe(0)
+  typeId = res.body.data.id
 })
 
-test('查询分类，应该成功', async () => {
+test('修改某个分类，应该成功', async () => {
   const res = await server
-    .get('/api/types')
+    .patch(`/api/types/${typeId}`)
+    .send({
+      title: '修改后的分类',
+      logo: 'http://www.baidu.com',
+      index: 999
+    })
     .set('Authorization', 'Bearer ' + token)
   expect(res.body.errno).toBe(0)
 })
 
+test('查询某个分类，应该成功', async () => {
+  const res = await server
+    .get(`/api/types/${typeId}`)
+    .set('Authorization', 'Bearer ' + token)
+  expect(res.body.errno).toBe(0)
+})
+
+test('查询所有分类，应该成功', async () => {
+  const res = await server
+    .get(`/api/types`)
+    .set('Authorization', 'Bearer ' + token)
+  expect(res.body.errno).toBe(0)
+})
+
+
+test('删除某个分类，应该成功', async () => {
+  const res = await server
+    .delete(`/api/types/${typeId}`)
+    .set('Authorization', 'Bearer ' + token)
+  expect(res.body.errno).toBe(0)
+})
