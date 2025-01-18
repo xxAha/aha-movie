634efc045333f7c9444ef753eeb94bbc133b7d31
zhucanfeng <223311795@qq.com>
2025-01-18 08:59:00 +0800

    test

--- test/server.js
+++ test/server.js
@@ -2,5 +2,5 @@ const request = require('supertest')
 //require('../src/app') -> 引入了koa的实例
 //.callback() -> 获取实例的每一次请求
 const server = require('../src/app').callback()
-
+//2025-01-18
 module.exports = request(server)
